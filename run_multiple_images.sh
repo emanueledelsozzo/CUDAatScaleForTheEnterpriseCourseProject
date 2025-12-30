ebf241b3183ee54d20c4f816d9e826f673331dde
Emanuele Del Sozzo <emanuele@30-9-192.wireless.csail.mit.edu>
2025-12-30 15:35:56 -0500

    updated run scripts

--- run_multiple_images.sh
+++ run_multiple_images.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-cd ../
 make clean build
 
 input_folder=datasets/aerials_pgm
--- run_single_image.sh
+++ run_single_image.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-cd ..
 make clean build
 
 ./bin/edgeDetector -input datasets/Lena.pgm -output datasets/Lena_edgeDetector.pgm
